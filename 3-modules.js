7245e1505db19942467d2b127349c62a85c2a23c
Thanh <thanh.thuonq09@gmail.com>
2025-08-20 16:30:00 -0500

    learning is fun!

---
+++ 3-modules.js
@@ -0,0 +1,8 @@
+//Every file is module by default
+//Modules - Encapsulated Code (only share minimum)
+
+const names = require('./4-name')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+
